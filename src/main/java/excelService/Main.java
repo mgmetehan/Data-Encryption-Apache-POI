974a652bd64db06635fb2f01b0845e38c5d608f9
Metehan Gultekin <mgmetehan@hotmail.com>
2022-06-09 15:59:05 +0300

    Create a copy of excel file function

--- src/main/java/excelService/Main.java
+++ src/main/java/excelService/Main.java
@@ -1,11 +1,26 @@
 package excelService;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Main {
     //Bir Hucrede boşluk bırakıp 2 adet tc yazarsan çalışmıyor
     public static void main(String[] args) {
-        String filePath = "C:/Users/mgmet/Desktop/deneme.xlsx";
+        String filePath = "C:\\Users\\mgmet\\Desktop\\deneme.xlsx";
+
+        File originalWb = new File("C:\\Users\\mgmet\\Desktop\\deneme.xlsx");
+        File clonedWb = new File("C:\\Users\\mgmet\\Desktop\\deneme_orjinal.xlsx");
+        try {
+            Files.copy(originalWb.toPath(), clonedWb.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         Reader r = new Reader();
         r.ReadCellData(filePath);
+        originalWb.renameTo(new File("C:\\Users\\mgmet\\Desktop\\deneme_mask.xlsx"));
+        clonedWb.renameTo(new File("C:\\Users\\mgmet\\Desktop\\deneme.xlsx"));
         //ör tc 22260299888 / 11118495268
     }
 }
