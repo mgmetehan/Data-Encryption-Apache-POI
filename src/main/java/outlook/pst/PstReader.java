7a6e7038016bf7810ed47b2818858862746ae5e8
Metehan Gultekin <mgmetehan@hotmail.com>
2022-06-30 15:15:04 +0300

    Update to PstReader class

--- src/main/java/outlook/pst/PstReader.java
+++ src/main/java/outlook/pst/PstReader.java
@@ -4,31 +4,24 @@ import com.pff.*;
 
 import java.util.*;
 
-public class pstRead {
+public class PstReader {
     public static void main(String[] args) {
         String path = "C:\\Users\\mgmet\\Desktop\\8.pst";
-        new pstRead(path);
+        new PstReader(path);
     }
 
-    public pstRead(String filename) {
+    public PstReader(String filename) {
         try {
             PSTFile pstFile = new PSTFile(filename);
-            System.out.println(pstFile.getMessageStore().getDisplayName());
+            System.out.println("Main Filename: " + pstFile.getMessageStore().getDisplayName());
             processFolder(pstFile.getRootFolder());
         } catch (Exception err) {
             err.printStackTrace();
         }
     }
 
-    int depth = -1;
-
     public void processFolder(PSTFolder folder) throws PSTException, java.io.IOException {
-        depth++;
-        // the root folder doesn't have a display name
-        if (depth > 0) {
-            printDepth();
-            System.out.println(folder.getDisplayName());
-        }
+        System.out.println("Child Filename: " + folder.getDisplayName());
 
         // go through the folders...
         if (folder.hasSubfolders()) {
@@ -40,22 +33,14 @@ public class pstRead {
 
         // and now the emails for this folder
         if (folder.getContentCount() > 0) {
-            depth++;
             PSTMessage email = (PSTMessage) folder.getNextChild();
             while (email != null) {
-                printDepth();
+                System.out.println("------------------------------------------");
                 System.out.println("Email: " + email.getSubject());
+                System.out.println("Body: " + email.getBody());
                 email = (PSTMessage) folder.getNextChild();
             }
-            depth--;
+            System.out.println("------------------------------------------");
         }
-        depth--;
-    }
-
-    public void printDepth() {
-        for (int x = 0; x < depth - 1; x++) {
-            System.out.print(" | ");
-        }
-        System.out.print(" |- ");
     }
 }
